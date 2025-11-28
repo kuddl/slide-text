9f6ee1aeec1d2d962f293b3f1468e73fb9e6e94a
kuddl <tobias.sailer@gmail.com>
2025-11-28 15:40:14 +0100

    slide 1

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+const sliderContainer = document.querySelector('.slider-container');
+const buttons = document.querySelectorAll('button');
+
+let current = 0;
+const sliderLength= sliderContainer.children.length || 1;
+buttons.forEach((button)=>{
+    button.addEventListener('click', (e) => {
+        // Scroll to the second slide (index 1)
+        
+        const dir = e.target.dataset.slideDirection || "prev"
+        if(dir === "prev") { current--}
+        if(dir === "next") { current++}
+console.log(dir,current)
+        // wrap around
+        if (current < 0) current = sliderLength-1;
+        if (current >= sliderLength) current = 0;     
+        
+        const secondSlide = sliderContainer.children[current] ;
+        secondSlide.scrollIntoView({ behavior: 'smooth' });
+    });
+})
